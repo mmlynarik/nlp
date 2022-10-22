877a9e6374514736a70ad891464ddc54708077f7
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-10-22 02:17:43 +0200

    Third commit

--- src/read_data.py
+++ src/read_data.py
@@ -1,9 +1,12 @@
-from datetime import date
 import json
+from datetime import date
+
 import pandas as pd
 
 f = open("data/train_reviews.json")
 reviews = json.load(f)
 
 df = pd.DataFrame(reviews)
+
+
 print(df)
