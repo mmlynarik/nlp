cf2cb98fc6d106b9271e394d1945637a538d066f
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-10-23 00:02:14 +0200

    settings

--- src/frontend/nlp/settings.py
+++ src/frontend/nlp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "okra",
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
