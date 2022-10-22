c1d20b2375ed41b530af473a4640e7d45bcd4de2
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-10-22 15:12:21 +0200

    Reorganize folder

--- src/read_data.py
+++ src/read_data.py
@@ -1,12 +1,12 @@
 import json
-from datetime import date
 
 import pandas as pd
 
 f = open("data/train_reviews.json")
 reviews = json.load(f)
 
-df = pd.DataFrame(reviews)
+df = pd.DataFrame(
+    reviews)
 
 
 print(df)
