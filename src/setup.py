0bc6b4a9f7a59139d08646c73a5e35c93a5205be
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-10-23 19:32:18 +0200

    Fix setup.py

---
+++ src/setup.py
@@ -0,0 +1,36 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+from pathlib import Path
+from setuptools import setup
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+print(f"{ROOT_DIR=}")
+with open(ROOT_DIR / "README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    package_data={},
+    packages=[
+        "okra",
+        "djangoproject",
+        "vacuumlabs",
+    ],
+    description="My NLP home projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "load_okra_data=okra.load_okra_data:main",
+        ]
+    },
+)
