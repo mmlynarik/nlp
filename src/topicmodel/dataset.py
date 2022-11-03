d4d026929f18e5a406b8a3eb3317039f96f3dcc0
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-11-03 10:25:05 +0100

    Fix

--- src/topicmodel/dataset.py
+++ src/topicmodel/dataset.py
@@ -2,5 +2,4 @@ import tensorflow as tf
 import numpy as np
 from keras.layers.preprocessing.text_vectorization import TextVectorization
 
-print(tf.config.list_physical_devices('GPU'))
-
+print(tf.config.list_physical_devices("GPU"))
