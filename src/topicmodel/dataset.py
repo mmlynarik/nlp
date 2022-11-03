190b17b6701b9fd5512e263c1eaceedc7b643f96
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-11-03 20:43:06 +0100

    Last

--- src/topicmodel/dataset.py
+++ src/topicmodel/dataset.py
@@ -24,16 +24,23 @@ data_out = np.arange(12).reshape(BATCH_SIZE, MAX_SEQ_LEN, LSTM_HIDDEN_DIM)
 
 
 class LSTMOkraModel(keras.Model):
-    def __init__(self):
+    def __init__(
+        self,
+        batch_size: int,
+        max_seq_len: int,
+        embedding_dim: int,
+        vocab_size: int,
+        dense_dim: int,
+        lstm_hidden_dim: int,
+    ):
         super().__init__()
-        self.input_layer = layers.Input(shape=(MAX_SEQ_LEN,), batch_size=BATCH_SIZE, name="token_ids")
-        self.embed = layers.Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True, name="embed")
-        self.dense = layers.Dense(DENSE_DIM, name="dense")
-        self.lstm = layers.LSTM(LSTM_HIDDEN_DIM, return_sequences=True, name="lstm")
-        self.out = self.call(self.input_layer)
+        self.embedding = layers.Embedding(vocab_size, embedding_dim, mask_zero=True, name="embed")
+        self.dense = layers.Dense(dense_dim, name="dense")
+        self.lstm = layers.LSTM(lstm_hidden_dim, return_sequences=True, name="lstm")
+        self.call(layers.Input(shape=(max_seq_len,), batch_size=batch_size))
 
     def call(self, inputs):
-        x = self.embed(inputs)
+        x = self.embedding(inputs)
         x = self.dense(x)
         x = self.lstm(x)
         return x
@@ -43,7 +50,14 @@ class LSTMOkraModel(keras.Model):
         return self.layers[-1].output_shape
 
 
-model = LSTMOkraModel()
+model = LSTMOkraModel(
+    batch_size=BATCH_SIZE,
+    max_seq_len=MAX_SEQ_LEN,
+    embedding_dim=EMBEDDING_DIM,
+    vocab_size=VOCAB_SIZE,
+    dense_dim=DENSE_DIM,
+    lstm_hidden_dim=LSTM_HIDDEN_DIM,
+)
 model.compile(loss="mse", optimizer="adam")
 preds = model.predict(token_ids)
 loss = model.evaluate(token_ids, data_out, verbose=0)
