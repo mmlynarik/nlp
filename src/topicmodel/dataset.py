4d69c618d6a040534b3b24d9fee3ad778f662501
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-11-03 16:45:31 +0100

    Final today

--- src/topicmodel/dataset.py
+++ src/topicmodel/dataset.py
@@ -9,21 +9,24 @@ tf.random.set_seed(1)
 
 MAX_SEQ_LEN = 4
 BATCH_SIZE = 1
+HIDDEN_DIM = 3
+EMBEDDING_DIM = 5
+VOCAB_SIZE = 8
 
-data_in = np.array([[1, 2, 0, 0]])
-data_out = np.arange(12).reshape(1, 4, 3)
+token_ids = np.array([[1, 2, 0, 0]])
+data_out = np.arange(12).reshape(BATCH_SIZE, MAX_SEQ_LEN, HIDDEN_DIM)
 
-input = layers.Input(shape=(MAX_SEQ_LEN,), batch_size=BATCH_SIZE)
-embed = layers.Embedding(input_dim=5, output_dim=5, mask_zero=True, input_length=4)(input)
+input = layers.Input(shape=(MAX_SEQ_LEN,), batch_size=BATCH_SIZE, name="token_ids")
+embed = layers.Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True, input_length=4)(input)
 dense = layers.Dense(3)(embed)
 output = layers.LSTM(3, return_sequences=True)(dense)
 
 model = models.Model(inputs=input, outputs=output)
 model.compile(loss="mse", optimizer="adam")
-preds = model.predict(data_in)
-loss = model.evaluate(data_in, data_out, verbose=0)
+preds = model.predict(token_ids)
+loss = model.evaluate(token_ids, data_out, verbose=0)
 
-print(preds)
+print("Predictions:\n", preds)
 print("Computed Loss:", loss)
 print("Recomputed Loss", np.sum(np.square(preds[0, :2] - data_out[0, :2])) / math.prod(output.shape[1:]))
 
