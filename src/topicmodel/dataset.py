f51b8fec22a71634cbd7ce96e603a3fee97a833c
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-11-03 17:39:22 +0100

    Final

--- src/topicmodel/dataset.py
+++ src/topicmodel/dataset.py
@@ -1,9 +1,10 @@
 import math
 
-import tensorflow as tf
+import keras
 import numpy as np
+import tensorflow as tf
 from keras import layers, models
-from keras.layers.preprocessing.text_vectorization import TextVectorization
+from keras.layers.preprocessing import text_vectorization as text
 
 tf.random.set_seed(1)
 
@@ -11,23 +12,43 @@ BATCH_SIZE = 1
 MAX_SEQ_LEN = 4
 VOCAB_SIZE = 8
 EMBEDDING_DIM = 5
-HIDDEN_DIM = 3
+DENSE_DIM = 3
+LSTM_HIDDEN_DIM = 3
+
+TV = text.TextVectorization()
 
 token_ids = np.array([[1, 2, 0, 0]])
-data_out = np.arange(12).reshape(BATCH_SIZE, MAX_SEQ_LEN, HIDDEN_DIM)
+data_out = np.arange(12).reshape(BATCH_SIZE, MAX_SEQ_LEN, LSTM_HIDDEN_DIM)
 
-input = layers.Input(shape=(MAX_SEQ_LEN,), batch_size=BATCH_SIZE, name="token_ids")
-embed = layers.Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True, input_length=4)(input)
-dense = layers.Dense(3)(embed)
-output = layers.LSTM(3, return_sequences=True)(dense)
+# Flatten layer does not propagate nor consume mask produced by Embedding layer.
 
-model = models.Model(inputs=input, outputs=output)
+
+class LSTMOkraModel(keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.embed = layers.Embedding(VOCAB_SIZE, EMBEDDING_DIM, mask_zero=True)
+        self.dense = layers.Dense(DENSE_DIM)
+        self.hidden = layers.LSTM(LSTM_HIDDEN_DIM, return_sequences=True)
+
+    def call(self, inputs):
+        x = self.embed(inputs)
+        x = self.dense(x)
+        x = self.hidden(x)
+        return x
+
+    def summary(self):
+        x = layers.Input(shape=(MAX_SEQ_LEN,), batch_size=BATCH_SIZE, name="token_ids")
+        model = keras.Model(inputs=[x], outputs=self.call(x))
+        return model.summary()
+
+
+model = LSTMOkraModel()
 model.compile(loss="mse", optimizer="adam")
 preds = model.predict(token_ids)
 loss = model.evaluate(token_ids, data_out, verbose=0)
 
 print("Predictions:\n", preds)
-print("Computed Loss:", loss)
-print("Recomputed Loss", np.sum(np.square(preds[0, :2] - data_out[0, :2])) / math.prod(output.shape[1:]))
+print("Loss:", loss)
+print("Loss (recalc):", np.sum(np.square(preds[0, :2] - data_out[0, :2])) / LSTM_HIDDEN_DIM / MAX_SEQ_LEN)
 
 model.summary()
