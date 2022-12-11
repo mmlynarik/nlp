9c2b6a60f72b2fa269da5cad65097e46ce8a0f9b
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-12-11 20:08:30 +0100

    New version of sentence splitter

--- src/topicmodel/datamodule/utils.py
+++ src/topicmodel/datamodule/utils.py
@@ -1,4 +1,4 @@
-import re
+import regex
 import warnings
 from datetime import date
 
@@ -10,9 +10,16 @@ from topicmodel.config import OKRA_DB
 from topicmodel.datamodule.queries import QUERY_OKRA_DATA_PG
 
 
-RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES = re.compile(r"(?<=[^A-Z].[.!?])\b(?=[A-Z])")
-RE_SPLITTER_SENTENCE = re.compile(r"(?<=[^A-Z].[.!?]) +(?=[A-Z])")
-RE_CONTEXT = re.compile(r".......")
+RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES = regex.compile(r"(?<=[^A-Z].[.!?])\b(?=[A-Z])")
+RE_SPLITTER_SENTENCE = regex.compile(r"(?<=[^A-Z].[.!?]) +(?=[A-Z])")
+RE_CONTEXT = regex.compile(r".......")
+
+RE_TYPO_BASE_DELIMITERS_ALIGNED_RIGHT = regex.compile(r"(?<=\w\w) ([.,?!]+)(?=\w\w)")
+RE_TYPO_BASE_DELIMITERS_SQUEEZED = regex.compile(r"(?<=\w\w)([.,?!]+)(?=\w\w)")
+RE_TYPO_BASE_DELIMITERS_CENTERED = regex.compile(r"(?<=\w\w) ([.,?!]+) (?=\w\w)")
+RE_TYPO_LOWERCASE_START_OF_SENTENCE = regex.compile(r"(?<=\w\w[.?!]+ )(\p{Ll})")
+RE_FIX_BASE_DELIMITERS = r"\1 "
+RE_FIX_UPPERCASE_START_OF_SENTENCE = lambda x: x.group(1).upper()
 
 
 def read_okra_data_from_db(date_from: date, date_to: date) -> pd.DataFrame:
@@ -23,14 +30,14 @@ def read_okra_data_from_db(date_from: date, date_to: date) -> pd.DataFrame:
     return df_okra
 
 
-def join_regexes(patterns: list[re.Pattern]):
+def join_regexes(patterns: list[regex.Pattern]):
     return "".join(x.pattern for x in patterns)
 
 
 def find_typo_unsplit_sentences(string: str) -> list[str]:
     """Find all typos with no whitespace between [.?!] and new sentence."""
     pattern = join_regexes([RE_CONTEXT, RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES, RE_CONTEXT])
-    return re.findall(pattern, string)
+    return regex.findall(pattern, string)
 
 
 def get_value_counts_of_typo_unsplit_sentences(df: pd.DataFrame, col="text") -> pd.Series:
@@ -38,23 +45,46 @@ def get_value_counts_of_typo_unsplit_sentences(df: pd.DataFrame, col="text") -> 
 
 
 def split_typo_unsplit_sentences(string: str) -> str:
-    """Find all cases of no whitespace between [.?!] and new sentence and fixes them by adding a space."""
-    return " ".join(re.split(RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES, string))
+    """Find all cases of no whitespace between [.?!] and new sentence and fix them by inserting a space."""
+    return " ".join(regex.split(RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES, string))
 
 
 def split_text_to_sentences_regex(rectified_string: str) -> list[str]:
     """First fix unsplit sentences typos and then split the text on correct end-of-sentences whitespace."""
-    rectified_string = split_typo_unsplit_sentences(rectified_string)
-    return re.split(RE_SPLITTER_SENTENCE, rectified_string)
+    rectified_string = rectify_base_typos(rectified_string)
+    return regex.split(RE_SPLITTER_SENTENCE, rectified_string)
 
 
 def expand_sentences_into_rows(df_data: pd.DataFrame, idcol: str, outcol: str) -> pd.DataFrame:
     output_data = []
     for _, row in df_data.iterrows():
         for idx, sentence in enumerate(row["sentences"]):
-            output_data.append({**row.to_dict(), idcol: row[idcol] + idx / 10, outcol: sentence})
+            output_data.append(
+                {
+                    **row.to_dict(),
+                    idcol: row[idcol] + (idx + 1) / 100,
+                    outcol: sentence,
+                    "length": len(sentence),
+                }
+            )
     return pd.DataFrame(output_data)
+
+
+def rectify_base_typos(string: str) -> str:
+    replacements = [
+        (RE_TYPO_BASE_DELIMITERS_ALIGNED_RIGHT, RE_FIX_BASE_DELIMITERS),
+        (RE_TYPO_BASE_DELIMITERS_SQUEEZED, RE_FIX_BASE_DELIMITERS),
+        (RE_TYPO_BASE_DELIMITERS_CENTERED, RE_FIX_BASE_DELIMITERS),
+        (RE_TYPO_LOWERCASE_START_OF_SENTENCE, RE_FIX_UPPERCASE_START_OF_SENTENCE),
+    ]
+    for typo, fix in replacements:
+        string = regex.sub(typo, fix, string)
+    return string
 
 
 def tf_decode(tensor: tf.Tensor):
     return tensor.numpy().decode("utf-8")
+
+
+# text = "Aaaaa!Ahoj , ako sa mas? mam sa ok!ahoj."
+# print(rectify_base_typos(text))
