fa3b0bac88f6b0827c72d08bb1444086576cb7ba
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-12-11 15:30:28 +0100

    Remove hand-crafted sentence splitter in favour of regex version

--- src/topicmodel/datamodule/utils.py
+++ src/topicmodel/datamodule/utils.py
@@ -10,6 +10,11 @@ from topicmodel.config import OKRA_DB
 from topicmodel.datamodule.queries import QUERY_OKRA_DATA_PG
 
 
+RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES = re.compile(r"(?<=[^A-Z].[.!?])\b(?=[A-Z])")
+RE_SPLITTER_SENTENCE = re.compile(r"(?<=[^A-Z].[.!?]) +(?=[A-Z])")
+RE_CONTEXT = re.compile(r".......")
+
+
 def read_okra_data_from_db(date_from: date, date_to: date) -> pd.DataFrame:
     with warnings.catch_warnings():  # ignore pandas issue #45660
         warnings.simplefilter("ignore", UserWarning)
@@ -18,55 +23,25 @@ def read_okra_data_from_db(date_from: date, date_to: date) -> pd.DataFrame:
     return df_okra
 
 
-def text_to_sentences(text: str) -> list[str]:
-    text = text + "." if text[-1].isalpha() or text[-1].isdigit() else text
-    alphabets = "([A-Za-z])"
-    prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
-    suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-    starters = (
-        "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
-    )
-    acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-    websites = "[.](com|net|org|io|gov|co|uk)"
-    digits = "([0-9])"
+def find_typo_unsplit_sentences(string: str) -> list[str]:
+    """Find all typos with no whitespace between [.?!] and new sentence."""
+    pattern = "".join(x.pattern for x in [RE_CONTEXT, RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES, RE_CONTEXT])
+    return re.findall(pattern, string)
 
-    text = " " + text + " "
-    text = text.replace("\n", " ")
-    text = re.sub(prefixes, "\\1<prd>", text)
-    text = re.sub(websites, "<prd>\\1", text)
-    text = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", text)
 
-    eos_strings = ["...", "..", "?!", "!?", "!!", "??", ".!", "!.", "?.", ". !"]
+def get_value_counts_of_typo_unsplit_sentences(df: pd.DataFrame, col="text") -> pd.Series:
+    return df[col].apply(lambda x: tuple(find_typo_unsplit_sentences(x))).value_counts()
 
-    for string in eos_strings:
-        if string in text:
-            text = text.replace(string, len(string) * "<prd>")
 
-    if "Ph.D" in text:
-        text = text.replace("Ph.D.", "Ph<prd>D<prd>")
-    text = re.sub("\s" + alphabets + "[.] ", " \\1<prd> ", text)
-    text = re.sub(acronyms + " " + starters, "\\1<stop> \\2", text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>\\3<prd>", text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]", "\\1<prd>\\2<prd>", text)
-    text = re.sub(" " + suffixes + "[.] " + starters, " \\1<stop> \\2", text)
-    text = re.sub(" " + suffixes + "[.]", " \\1<prd>", text)
-    text = re.sub(" " + alphabets + "[.]", " \\1<prd>", text)
-    if "”" in text:
-        text = text.replace(".”", "”.")
-    if '"' in text:
-        text = text.replace('."', '".')
-    if "!" in text:
-        text = text.replace('!"', '"!')
-    if "?" in text:
-        text = text.replace('?"', '"?')
-    text = text.replace(".", ".<stop>")
-    text = text.replace("?", "?<stop>")
-    text = text.replace("!", "!<stop>")
-    text = text.replace("<prd>", ".")
-    sentences = text.split("<stop>")
-    sentences = sentences[:-1]
-    sentences = [s.strip() for s in sentences]
-    return sentences
+def split_typo_unsplit_sentences(string: str) -> str:
+    """Find all cases of no whitespace between [.?!] and new sentence and fixes them by adding a space."""
+    return " ".join(re.split(RE_SPLITTER_TEXT_ON_TYPO_UNSPLIT_SENTENCES, string))
+
+
+def split_text_to_sentences_regex(string: str) -> list[str]:
+    """First fix unsplit sentences typos and then split the text on correct end-of-sentences whitespace."""
+    string = split_typo_unsplit_sentences(string)
+    return re.split(RE_SPLITTER_SENTENCE, string)
 
 
 def expand_sentences_into_rows(df_data: pd.DataFrame, idcol: str, outcol: str) -> pd.DataFrame:
@@ -79,3 +54,10 @@ def expand_sentences_into_rows(df_data: pd.DataFrame, idcol: str, outcol: str) -
 
 def tf_decode(tensor: tf.Tensor):
     return tensor.numpy().decode("utf-8")
+
+
+old_text = "Hello, how are you!I'm fine, thanks!And you?"
+text = """On 10th May I received a refund from Virgin for a trip to Preston from Edinburgh on 22nd April which had arrived over 2 hours late.Unfortunately there were 5 of us on the trip but the refund was only for 1 ticket.I immediately went on live chat as requested but they said they couldn't deal with that and I should email in.I also tried phoning but it just kept ringing.Since then I have emailed twice but only get automated responses and then nothing.The letter I received is very pleased with itself for dealing with my complaint so promptly .If only it was dealt with as I am due to refund money to the others and can't get it finished with.If there was a decent alternative I would never use this lot again as trains I get are invariably late."""
+
+print(split_text_to_sentences(old_text), "\n")
+print(split_text_to_sentences_regex(old_text), "\n")
