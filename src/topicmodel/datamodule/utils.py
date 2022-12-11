a5490a20db8151cf52c93a18ab9a51d36d6ab04b
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-12-11 20:26:25 +0100

    Finalize regex utils

--- src/topicmodel/datamodule/utils.py
+++ src/topicmodel/datamodule/utils.py
@@ -17,9 +17,12 @@ RE_CONTEXT = regex.compile(r".......")
 RE_TYPO_BASE_DELIMITERS_ALIGNED_RIGHT = regex.compile(r"(?<=\w\w) ([.,?!]+)(?=\w\w)")
 RE_TYPO_BASE_DELIMITERS_SQUEEZED = regex.compile(r"(?<=\w\w)([.,?!]+)(?=\w\w)")
 RE_TYPO_BASE_DELIMITERS_CENTERED = regex.compile(r"(?<=\w\w) ([.,?!]+) (?=\w\w)")
+RE_TYPO_MULTIPLE_SPACE = regex.compile(r" +")
 RE_TYPO_LOWERCASE_START_OF_SENTENCE = regex.compile(r"(?<=\w\w[.?!]+ )(\p{Ll})")
+
 RE_FIX_BASE_DELIMITERS = r"\1 "
 RE_FIX_UPPERCASE_START_OF_SENTENCE = lambda x: x.group(1).upper()
+RE_FIX_SINGLE_SPACE = r" "
 
 
 def read_okra_data_from_db(date_from: date, date_to: date) -> pd.DataFrame:
@@ -50,7 +53,7 @@ def split_typo_unsplit_sentences(string: str) -> str:
 
 
 def split_text_to_sentences_regex(rectified_string: str) -> list[str]:
-    """First fix unsplit sentences typos and then split the text on correct end-of-sentences whitespace."""
+    """First fix end-of-sentences typos and then split the text on correct end-of-sentences whitespace."""
     rectified_string = rectify_base_typos(rectified_string)
     return regex.split(RE_SPLITTER_SENTENCE, rectified_string)
 
@@ -75,6 +78,7 @@ def rectify_base_typos(string: str) -> str:
         (RE_TYPO_BASE_DELIMITERS_ALIGNED_RIGHT, RE_FIX_BASE_DELIMITERS),
         (RE_TYPO_BASE_DELIMITERS_SQUEEZED, RE_FIX_BASE_DELIMITERS),
         (RE_TYPO_BASE_DELIMITERS_CENTERED, RE_FIX_BASE_DELIMITERS),
+        (RE_TYPO_MULTIPLE_SPACE, RE_FIX_SINGLE_SPACE),
         (RE_TYPO_LOWERCASE_START_OF_SENTENCE, RE_FIX_UPPERCASE_START_OF_SENTENCE),
     ]
     for typo, fix in replacements:
@@ -84,7 +88,3 @@ def rectify_base_typos(string: str) -> str:
 
 def tf_decode(tensor: tf.Tensor):
     return tensor.numpy().decode("utf-8")
-
-
-# text = "Aaaaa!Ahoj , ako sa mas? mam sa ok!ahoj."
-# print(rectify_base_typos(text))
