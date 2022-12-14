1a11e6794c312192b9544eec3705f9226ccef905
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-12-14 09:21:46 +0100

    New dataset functions

--- src/topicmodel/datamodule/dataset.py
+++ src/topicmodel/datamodule/dataset.py
@@ -1,18 +1,40 @@
-import numpy as np
 import tensorflow as tf
 from keras.preprocessing.sequence import skipgrams
 from keras.layers.preprocessing import text_vectorization as text
 
 
-class OKRAWord2VecSentenceDataset(tf.data.Dataset):
-    """Sentence-level dataset class used for traning Word2Vec model."""
+class OKRAWord2VecStringSentenceDataset:
+    """Sentence-level string dataset class."""
 
     def __new__(cls, data: tuple[tf.Tensor, tf.Tensor, tf.Tensor]):
         return tf.data.Dataset.from_tensor_slices(data)
 
 
-class OKRAWord2VecDataLoader(tf.data.Dataset):
-    def __new__(cls, dataset: tf.data.Dataset, batch_size: int):
-        batched_dataset = dataset.batch(batch_size)
-        # TODO: Add .map() transformations here e.g. to tokenize the input strings
-        return batched_dataset
+class OKRAWord2VecEncodedSentenceDataset:
+    """Sentence-level integer-encoded dataset class."""
+
+    def __new__(cls, data: tuple[tf.Tensor, tf.Tensor, tf.Tensor]):
+        return tf.data.Dataset.from_tensor_slices(data)
+
+
+def get_keys_tensor(dataset: tf.data.Dataset) -> tf.Tensor:
+    return tf.constant([item for item in dataset.map(lambda key, x, y: key).as_numpy_iterator()])
+
+
+def get_outputs_tensor(dataset: tf.data.Dataset) -> tf.Tensor:
+    return tf.constant([item for item in dataset.map(lambda key, x, y: y).as_numpy_iterator()])
+
+
+def get_corpus_tensor(dataset: tf.data.Dataset) -> tf.Tensor:
+    return tf.constant([item for item in dataset.map(lambda key, x, y: x).as_numpy_iterator()])
+
+
+def get_corpus_dataset(dataset: tf.data.Dataset) -> tf.data.Dataset:
+    return dataset.map(lambda key, x, y: x)
+
+
+def get_printable_string_dataset(dataset: tf.data.Dataset) -> list[dict]:
+    return [
+        {"key": key.decode(), "review": x.decode(), "output": y.decode()}
+        for key, x, y in dataset.as_numpy_iterator()
+    ]
