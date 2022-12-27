fa1924b81c7765f5db104380236ea86b9f7710e4
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2022-12-27 17:22:02 +0100

    Add new regexes

--- src/topicmodel/datamodule/utils.py
+++ src/topicmodel/datamodule/utils.py
@@ -15,11 +15,17 @@ RE_TYPO_BASE_DELIMITERS_AFTER_SPACE = regex.compile(r"(?<=\p{L}\p{L}) ([.,?!]+)"
 RE_TYPO_BASE_DELIMITERS_SQUEEZED = regex.compile(r"(?<=\p{L}\p{L})([.,?!]+)(?=\p{L}\p{L})")
 RE_TYPO_BASE_DELIMITERS_CENTERED = regex.compile(r"(?<=\p{L}\p{L}) ([.,?!]+) (?=\p{L}\p{L})")
 RE_TYPO_MULTIPLE_SPACE = regex.compile(r" +")
-RE_TYPO_LOWERCASE_START_OF_SENTENCE = regex.compile(r"(?<=\w\w[.?!]+ )(\p{Ll})")
+RE_TYPO_LOWERCASE_START_OF_SENTENCE = regex.compile(r"(?<=\p{L}\p{L}[.?!] )(\p{Ll})")
+RE_MULTIPLE_EMOTION_DELIMITERS = regex.compile(r"([!?]+)")
 
 RE_FIX_BASE_DELIMITERS = r"\1 "
 RE_FIX_UPPERCASE_START_OF_SENTENCE = lambda x: x.group(1).upper()
 RE_FIX_SINGLE_SPACE = r" "
+RE_FIX_SINGLE_EMOTION_DELIMITER = lambda x: x.group(1)[0]
+
+RE_EN_TIME = r"(?<=\b)(\d?\d[.:]\d\d ?([ap]m))|(\d?\d[.:]\d\d)|(\d?\d ?([ap]m))(?=\b)"
+RE_EN_CCY = r"\p{Sc}\d+(,\d+)*(\.\d\d)?"
+RE_EN_PERIOD = r"\d+\.?\d? ?(days?|months?|years?|hours?|minutes?|weeks?|mins?|hrs?)"
 
 
 def read_okra_data_from_db(date_from: date, date_to: date) -> pd.DataFrame:
@@ -34,23 +40,46 @@ def join_regexes(patterns: list[regex.Pattern]):
     return "".join(x.pattern for x in patterns)
 
 
-def rectify_base_typos(string: str) -> str:
+def rectify_typos(string: str) -> str:
+    """Fix typos in text regarding delimiters and lowercase start of sentence."""
     replacements = [
         (RE_TYPO_BASE_DELIMITERS_AFTER_SPACE, RE_FIX_BASE_DELIMITERS),
         (RE_TYPO_BASE_DELIMITERS_SQUEEZED, RE_FIX_BASE_DELIMITERS),
         (RE_TYPO_BASE_DELIMITERS_CENTERED, RE_FIX_BASE_DELIMITERS),
         (RE_TYPO_MULTIPLE_SPACE, RE_FIX_SINGLE_SPACE),
         (RE_TYPO_LOWERCASE_START_OF_SENTENCE, RE_FIX_UPPERCASE_START_OF_SENTENCE),
+        (RE_MULTIPLE_EMOTION_DELIMITERS, RE_FIX_SINGLE_EMOTION_DELIMITER),
     ]
     for typo, fix in replacements:
         string = regex.sub(typo, fix, string)
     return string
 
 
-def split_text_to_sentences_regex(rectified_string: str) -> list[str]:
-    """First fix end-of-sentences typos and then split the text on correct end-of-sentences whitespace."""
-    rectified_string = rectify_base_typos(rectified_string)
-    return regex.split(RE_SPLITTER_SENTENCE, rectified_string)
+def mask_ccy(string: str, token="[CCY]") -> str:
+    """Replace currency expressions with a special token to reduce vocab size."""
+    return regex.sub(RE_EN_CCY, token, string)
+
+
+def mask_time(string: str, token="[TIME]") -> str:
+    """Replace time expressions in text with a special token to reduce vocab size."""
+    return regex.sub(RE_EN_TIME, token, string)
+
+
+def mask_period(string: str, token="[PERIOD]") -> str:
+    return regex.sub(RE_EN_PERIOD, token, string)
+
+
+def mask_symbols(string: str) -> str:
+    return mask_period(mask_time(mask_ccy(string)))
+
+
+def preprocess(string: str) -> str:
+    return mask_symbols(rectify_typos(string))
+
+
+def split_text_to_sentences(string: str) -> list[str]:
+    """Split the text on correct end-of-sentences delimiters."""
+    return regex.split(RE_SPLITTER_SENTENCE, string)
 
 
 def expand_sentences_into_rows(df_data: pd.DataFrame, idcol: str, outcol: str) -> pd.DataFrame:
