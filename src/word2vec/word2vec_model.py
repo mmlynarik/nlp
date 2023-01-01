f052c7945ff71ce307ca66d1438c9a6560210b6b
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-01-02 00:29:50 +0100

    Integrate Word2Vec model

--- src/word2vec/word2vec_model.py
+++ src/word2vec/word2vec_model.py
@@ -23,9 +23,14 @@ class Word2Vec(keras.Model):
     def __init__(self, vocab_size: int, embedding_dim: int, num_neg_samples: int):
         super().__init__()
         self.target_embedding = layers.Embedding(
-            vocab_size, embedding_dim, input_length=1, name="word_embeddings"
+            input_dim=vocab_size, output_dim=embedding_dim, input_length=1, name="word_embeddings"
         )
-        self.context_embedding = layers.Embedding(vocab_size, embedding_dim, input_length=num_neg_samples + 1)
+        self.context_embedding = layers.Embedding(
+            input_dim=vocab_size,
+            output_dim=embedding_dim,
+            input_length=num_neg_samples + 1,
+            name="context_embeddings",
+        )
         # self.call(layers.Input(shape=(,)))
 
     def call(self, input_tensor_pair):
