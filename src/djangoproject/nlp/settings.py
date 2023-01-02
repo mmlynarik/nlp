9f18f0c4ac0701b9e9bd92f9acda3edd641c6930
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-01-02 08:54:30 +0100

    Integrate Word2Vec model

--- src/djangoproject/nlp/settings.py
+++ src/djangoproject/nlp/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-dh94tan1tlo*7bil3v(qu9wo=b!&8o_t3n&x(ohl8!o+f_nyms"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
