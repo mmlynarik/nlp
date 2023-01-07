d662a9553444dc349e1d7b29ccf58e042f9bf477
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2023-01-07 16:23:16 +0100

    Rename model

--- src/word2vec/model.py
+++ src/word2vec/model.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from keras import layers, optimizers
 
 
-class Word2Vec(keras.Model):
+class Word2VecModel(keras.Model):
     def __init__(self, vocab_size: int, embedding_dim: int, num_neg_samples: int):
         super().__init__()
         self.target_embedding = layers.Embedding(vocab_size, embedding_dim, input_length=1, name="embeddings")
